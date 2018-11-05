5dfd62de5c903524e1501d6812e0ac55a90a1a0d
kcharler <kou17002@byui.edu>
2018-11-05 17:52:38 +0000

    update

--- assignments/Lesson-7/js/windchill.js
+++ assignments/Lesson-7/js/windchill.js
@@ -1,6 +1,6 @@
 
- var t = document.getElementById("temp").Value;
-    var s = document.getElementById("speed").Value;
+    var t = 76; 
+    var s = 5;
     var chill = 35.74 + 0.6215 * t - 35.75 * (Math.pow(s, 0.16)) + 0.4275 * t * Math.pow(s, 0.16)
     chill = Math.round(chill) + "°F"
 
