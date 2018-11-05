938708c01b4f4d02b11999e9aa687ed08e88dcbf
kcharler <kou17002@byui.edu>
2018-11-05 17:52:22 +0000

    update

--- assignments/Lesson-7/js/windchill.js
+++ assignments/Lesson-7/js/windchill.js
@@ -1,9 +1,7 @@
-function calcuateWindChillFactor(temperature, windSpeed) {
-    var t = 76 ;  /* temperature in Fahrenheit*/
-    var s = 5; /* wind speed in miles per hour*/
 
-    var chill = 35.74 + 0.6215*t - 35.75 * (Math.pow(wind, 0.16))  + 0.4275 * t * Math.pow(wind, 0.16)
-    chill = Math.round(chill) + "°F" 
+ var t = document.getElementById("temp").Value;
+    var s = document.getElementById("speed").Value;
+    var chill = 35.74 + 0.6215 * t - 35.75 * (Math.pow(s, 0.16)) + 0.4275 * t * Math.pow(s, 0.16)
+    chill = Math.round(chill) + "°F"
 
-    document.getElementById("windchill").innerHTML = chill;
-}
+    document.getElementById("windchill").innerHTML = chill;    
