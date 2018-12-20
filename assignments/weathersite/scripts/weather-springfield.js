37d52eb05cfd5460de5bcb6775bffd7cb948aea9
kcharler <kou17002@byui.edu>
2018-12-20 13:42:53 +0000

    update

---
+++ assignments/weathersite/scripts/weather-springfield.js
@@ -0,0 +1,24 @@
+// CONNECTION TO API //
+
+let weatherRequest = new XMLHttpRequest();
+let apiURL = 'https://api.openweathermap.org/data/2.5/weather?&id=4250542&units=imperial&APPID=826efe9c55de5d2734cfce7a148b4eac';
+weatherRequest.open('GET', apiURL, true);
+weatherRequest.send();
+
+weatherRequest.onload = function() {
+    let weatherData = JSON.parse(weatherRequest.responseText);
+    
+    document.getElementById('currenttemp-s').innerHTML = Math.round(weatherData.main.temp_max);
+    
+    document.getElementById('humidity').innerHTML = weatherData.main.humidity;
+    
+    document.getElementById('windspeed').innerHTML = Math.round(weatherData.wind.speed);
+    
+    document.getElementById('clouds').innerHTML = weatherData.clouds.all;
+    
+    document.getElementById('currentCond-s').innerHTML = weatherData.weather[0].description;
+    
+    let imagesrc = 'http://openweathermap.org/img/w/' + weatherData.weather[0].icon + '.png';
+    document.getElementById('weatherimage-s').src = imagesrc;
+    
+}
