ece4781a99b73d1398c21d98c98f1c620caba1c1
kcharler <kou17002@byui.edu>
2018-12-20 15:29:39 +0000

    complete

--- assignments/weathersite/js/weather-fish.js
+++ assignments/weathersite/js/weather-fish.js
@@ -1,14 +1,14 @@
 // CONNECTION TO API //
 
 let weatherRequest = new XMLHttpRequest();
-let apiURL = 'https://api.openweathermap.org/data/2.5/weather?&id=4156210&units=imperial&APPID=826efe9c55de5d2734cfce7a148b4eac';
+let apiURL = 'https://api.openweathermap.org/data/2.5/weather?&id=4428475&units=imperial&APPID=826efe9c55de5d2734cfce7a148b4eac';
 weatherRequest.open('GET', apiURL, true);
 weatherRequest.send();
 
 weatherRequest.onload = function() {
     let weatherData = JSON.parse(weatherRequest.responseText);
     
-    document.getElementById('currenttemp-f').innerHTML = Math.round(weatherData.main.temp_max);
+    document.getElementById('currenttemp-g').innerHTML = Math.round(weatherData.main.temp_max);
     
     document.getElementById('humidity').innerHTML = weatherData.main.humidity;
     
@@ -16,9 +16,9 @@ weatherRequest.onload = function() {
     
     document.getElementById('clouds').innerHTML = weatherData.clouds.all;
     
-    document.getElementById('currentCond-f').innerHTML = weatherData.weather[0].description;
+    document.getElementById('currentCond-g').innerHTML = weatherData.weather[0].description;
     
     let imagesrc = 'http://openweathermap.org/img/w/' + weatherData.weather[0].icon + '.png';
-    document.getElementById('weatherimage-f').src = imagesrc;
+    document.getElementById('weatherimage-g').src = imagesrc;
     
-}// end of the function
+}
